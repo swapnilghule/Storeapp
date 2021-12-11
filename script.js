162f2e34b57f83874967434bd3b7d76ca542d2d6
Swapnil Ghule <67332665+swapnilghule@users.noreply.github.com>
2021-12-11 20:45:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function insertNewRecord(data){
     var cell4 = newRow.insertCell(3);
         cell4.innerHTML = data.perPrice;
     var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+        cell5.innerHTML = `<button id="OnEditButton" onClick='onEdit(this)'>Edit</button> <button id="OnDeleteButton" onClick='onDelete(this)'>Delete</button>`
 }
 
 //Edit the data
