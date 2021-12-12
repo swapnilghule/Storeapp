085a9c9d42d05ff1eccb172d842eb1d3f47c50bc
Swapnil Ghule <67332665+swapnilghule@users.noreply.github.com>
2021-12-12 10:55:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,10 +58,10 @@ function updateRecord(formData){
 
 //Delete the data
 function onDelete(td){
-    if(confirm('Do you want to delete this record?')){
+   
         row = td.parentElement.parentElement;
         document.getElementById('storeList').deleteRow(row.rowIndex);
-    }
+    
     resetForm();
 }
 
